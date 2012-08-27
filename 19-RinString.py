ac2be5a823173af6a211f0c67396e630c45dc710
Fox.Lee <jiaqing.li@outlook.com>
2012-08-27 17:02:02 +0800

    R in string

---
+++ 19-RinString.py
@@ -0,0 +1,3 @@
+# If you need to specify some strings where no special processing such as escape sequences are handled, then what you need is to specify a raw string by prefixing r or R to the string.
+
+print (R"this 's new line with \n")
