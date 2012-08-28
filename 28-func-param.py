467ca6f1fbf1652e1f7fdaf6537e73fe1b99664b
Fox.Lee <jiaqing.li@outlook.com>
2012-08-28 15:21:04 +0800

    带参数的函数

---
+++ 28-func-param.py
@@ -0,0 +1,18 @@
+def printMax(a,b):
+     if a > b:
+          print(a, 'is max')
+     elif a==b:
+          print (a, ' is equal to ',b)
+     else:
+          print (b, 'is max')
+
+
+
+
+printMax (3,7)
+printMax (2,2)
+x=5
+y=3
+
+
+printMax (x,y)
