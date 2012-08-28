53d8c5f7a62457037145de91ec17fa45e4506d89
Fox.Lee <jiaqing.li@outlook.com>
2012-08-28 13:58:16 +0800

    format 的用法
    format 的用法

---
+++ 20-str-format.py
@@ -0,0 +1,8 @@
+age =25
+name = 'Swaroop'
+
+print ('{0} is {1} years old'.format(name,age))
+print ('Why is {0} playing with that python'.format(name))
+
+print ('{0:.3}'.format(1/3)) # 输出1/3的值, 精确到小数点后三位
+print ('{0:_^11}'.format('Hello')) #将 Hello 在__符号中居中(^)显示 总共11个字符 :___Hello___
