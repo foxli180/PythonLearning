490dc8b0071f47fadd91b5e46219267d5778712b
Fox.Lee <jiaqing.li@outlook.com>
2012-08-28 14:02:30 +0800

    几种不同的打印方式
    \ 转义 换行后所有三行文字会连为一行

---
+++ 21-var.py
@@ -0,0 +1,22 @@
+i = 5
+print (i)
+i = i + 1
+print (i)
+
+s = ''' this is a multi line
+text
+this is 3rd line'''
+
+print (s)
+
+s = """ this is a multi line
+text
+this is 3rd line"""
+
+print (s)
+
+s = 'this is a multi line\
+text\
+this is 3rd line'
+
+print (s)
