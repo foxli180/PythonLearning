956d380b17b72a9835a76349e2e4a5c96bbd1dc2
Fox.Lee <jiaqing.li@outlook.com>
2012-09-12 15:11:07 +0800

    String
    元组和系列的转换
    maketrans 似乎3.0 没有了

---
+++ 49-strings.py
@@ -0,0 +1,9 @@
+website = 'http://www.baidu.com'
+print (website)
+# website[-3:] = 'org' #这里会报错,string 应该是被当成了 元组而不是序列
+y = list (website) #转化为序列
+y[-3:] ='org'
+print (y)
+y = tuple(y) #再转化为元组
+print (y)
+print (website) # y =list(website) 和y =website 不一样,前者应该是创建新的内存空间,后者应该是指向同一片内存空间
---
+++ 50-translate.py
@@ -0,0 +1,3 @@
+from string import maketrans
+table = maketrans('cs', 'kz')
+print(table)
